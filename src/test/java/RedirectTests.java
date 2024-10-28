998b8f2a9bbd47faec12d949181075c4b21f4bcf
Anastasiya663 <a.konowalowa2010@yandex.ru>
2024-10-28 20:29:57 +0300

    Ex6: Редирект

---
+++ src/test/java/RedirectTests.java
@@ -0,0 +1,37 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class RedirectTests {
+
+    @Test
+    void testRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        int statusCode = response.getStatusCode();
+
+        System.out.println(locationHeader);
+
+        while (statusCode != 200) {
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(locationHeader)
+                    .andReturn();
+
+            locationHeader = response.getHeader("Location");
+            statusCode = response.getStatusCode();
+
+            System.out.println(locationHeader);
+        }
+    }
+}
