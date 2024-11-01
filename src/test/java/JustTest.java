009a6143e18383c771af47c62169912e68a067c9
Anastasiya663 <a.konowalowa2010@yandex.ru>
2024-11-01 17:25:05 +0300

    Ex10: Тест на короткую фразу

---
+++ src/test/java/JustTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JustTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "Hello, world", "Welcome, my friend!", "123456789012345"})
+    void testString(String s) {
+        assertTrue(s.length() > 15,  "Length of string less than 15");
+    }
+}
